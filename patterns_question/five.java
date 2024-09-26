03664a22207bacd86dd45c0688a8736731d48e92
Inchana A <153069497+Inchanaa@users.noreply.github.com>
2024-09-26 15:26:56 +0000

    patterns

--- patterns_question/five.java
+++ patterns_question/five.java
@@ -4,7 +4,7 @@ public class five{
     public static void main(String[] args){
       int n = 5, m = 5, i,j;
       for(i=n; i>=0; i--){
-        for(j=m; j>=i-n; j--){
+        for(j=m; j>=n-i; j--){
             // if(i<=j){
             System.out.print("*");
         // }
---
+++ patterns_question/prime.java
@@ -0,0 +1,16 @@
+package patterns_question;
+import java.util.Scanner;
+
+public class prime{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+                System.out.println("enter prime number");
+        int n=sc.nextInt();
+        System.out.println("1\n2\n3" );
+        for(int i=4; i<=n; i++){
+        if(i%i==0 && i%2!=0 && i%3!=0 ){
+        System.out.println(i);
+        }
+        }
+    }
+}
