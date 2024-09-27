86998828f8be5f137017c6c9e19d38ba8c36f2bb
Inchana A <153069497+Inchanaa@users.noreply.github.com>
2024-09-27 10:41:51 +0000

    Transposing

---
+++ arrays/Transposing.java
@@ -0,0 +1,40 @@
+// displan an array and search an element in the array and display its index
+
+package arrays;
+import java.util.Scanner;
+
+public class Transposing{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size");
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+
+        System.out.println("Enter an array");
+        int[][] a = new int[row][col];
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[i].length;j++){
+            a[i][j] = sc.nextInt();
+        }
+        }
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+            System.out.print(a[i][j]+" ");
+        }
+      System.out.println();
+        }
+        int[][] t = new int[col][row];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                t[j][i] = a[i][j];
+            }
+        }
+        System.out.println("Transposed Matrix:");
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < row; j++) {
+                System.out.print(t[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
