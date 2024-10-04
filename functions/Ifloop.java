b07470dd8e91345f43c28660c30dee54fb89c49c
Inchana A <153069497+Inchanaa@users.noreply.github.com>
2024-10-04 06:11:15 +0000

    if

---
+++ functions/Ifloop.java
@@ -0,0 +1,16 @@
+// Write a function that takes in age as input and returns if that person is eligible to vote or not.
+//  A person of age > 18 is eligible to vote.
+
+package functions;
+
+public class Ifloop{
+    public static int age(int n){
+        if(n > 18)
+        return True;
+        else
+        return False;
+    }
+    public static void main(String[] args){
+
+    }
+}
