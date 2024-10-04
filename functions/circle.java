64bb0ead742cf2d80b5f51fb6c252962da7bb64d
Inchana A <153069497+Inchanaa@users.noreply.github.com>
2024-10-04 06:01:11 +0000

    circle

--- functions/circle.java
+++ functions/circle.java
@@ -4,15 +4,15 @@ package functions;
 import java.util.*;
 
 public class circle{
-    final float pi = 3.14;
     public static void circumference(int n){
-        int cir = 2*pi*n;
+        final double pi = 3.14;
+        double cir = 2*pi*n;
         System.out.print(cir);
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
+        circumference(n);
     }
 
 }
