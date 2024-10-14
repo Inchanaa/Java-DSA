cb03c96f51b59aee426ec7a9ef53f29e41d95bf4
Inchana A <153069497+Inchanaa@users.noreply.github.com>
2024-10-14 15:30:58 +0000

    reverse a string

--- arraysAndstrings/reverseString.java
+++ arraysAndstrings/reverseString.java
@@ -5,17 +5,21 @@ import java.util.*;
 
  
 public class reverseString{
-    public static void rev(String str){
-        for(int i=str.length()-1;i>=0;i--){
-            System.out.print(str.charAt(i));
-        }
-    }
+    // public static void rev(String str){
+    //     for(int i=str.length()-1;i>=0;i--){
+    //         System.out.print(str.charAt(i));
+    //     }
+    // }
  
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        // String s = rev(str);
-        // System.out.print(s);
-        rev(str);
+        // str = str.replaceAll("\\s+","");
+        for(int i=str.length()-1;i>=0;i--){
+             
+            System.out.print(str.charAt(i));
+        }
+        // rev(str);
+        System.out.println();
     }
 }
