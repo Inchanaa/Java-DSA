b598531203ef72860480cabe43d6578d1bfab253
Inchana A <153069497+Inchanaa@users.noreply.github.com>
2024-10-15 06:48:47 +0000

    palindrome

--- arraysAndstrings/palindrome.java
+++ arraysAndstrings/palindrome.java
@@ -7,19 +7,15 @@ import java.util.Scanner;
 public class palindrome{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
+        String str =sc.nextLine();
         String rev = new StringBuilder(str).reverse().toString();
-        // for(int i=str.length()-1;i>=0;i--){
-        //     rev = Character.toString(str.charAt(i));
-
-        // }
-        // System.out.println(i);
-        if(str==rev){
+          System.out.println(rev);
+        if(str.equals(rev)){
             System.out.println(str+ " is palindrome");
         }
         else
         {
-            System.out.println(str+ " is not palindromw");
+            System.out.println(str+ " is not palindrome");
         }
-    }
+}
 }
