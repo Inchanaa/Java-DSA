f693a00c42f9a938c86a543a40d12a2d0f822037
Inchana A <153069497+Inchanaa@users.noreply.github.com>
2024-10-20 09:14:54 +0000

    if loop

--- functions/Ifloop.java
+++ functions/Ifloop.java
@@ -3,14 +3,23 @@
 
 package functions;
 
+import java.util.Scanner;
+
 public class Ifloop{
-    public static int age(int n){
+    public static boolean age(int n){
         if(n > 18)
-        return True;
+        return true;
         else
-        return False;
+        return false;
     }
     public static void main(String[] args){
-
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        if(age(n)==true){
+        System.out.println("can vote");
+    }
+        else
+        System.out.println("can not vote");
     }
 }
