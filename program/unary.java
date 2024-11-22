5485602fc157374ff2a6c099940a508b18cacd82
Inchana A <153069497+Inchanaa@users.noreply.github.com>
2024-11-22 07:58:26 +0000

    unary

--- program/unary.java
+++ program/unary.java
@@ -1,7 +1,7 @@
 public class unary {
 
     public static void main(String[] args) {
-
+//post increment
         // a++     -> first --> change value a++=11
         //         -> step 2 --> use value    
 
@@ -10,6 +10,7 @@ public class unary {
         System.out.println(a);              //11
         System.out.println(b);              //10
 
+        //pre increment
         // ++a        -> step 1 --> use value    
          //      ->      step 2 --> change value 
 
@@ -17,6 +18,25 @@ public class unary {
         d = ++c;
         System.out.println(c);              //11
         System.out.println(d);              //11
+
+
+        //post decrement
+        // a++     -> first --> change value a++=11
+        //         -> step 2 --> use value    
+
+        int e = 10, f= 0;
+        f = e--;
+        System.out.println(e);              //9
+        System.out.println(f);              //10
+
+        //pre decrement
+        // ++a        -> step 1 --> use value    
+         //      ->      step 2 --> change value 
+
+       int  g = 10, h= 0;
+        h = --g;
+        System.out.println(g);              //9
+        System.out.println(h);              //9
 
     }
 }
