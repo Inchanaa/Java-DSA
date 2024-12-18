7e15e681669e69c4aa746e4a68f7332b59adb8b1
Inchana A <153069497+Inchanaa@users.noreply.github.com>
2024-12-18 05:23:43 +0000

    parameter constructor

---
+++ OOPS/ParameterCons.java
@@ -0,0 +1,7 @@
+package OOPS;
+
+public class ParameterCons {
+    public static void main(String[] args) {
+        Student s = new Student("anu", 30);
+    }
+}
--- OOPS/Student.java
+++ OOPS/Student.java
@@ -1,24 +1,16 @@
-//constructor which has parameter -- parameterized constructor
-
 package OOPS;
 
-class Student{
+class Student {
     String name;
     int id;
     // void st(){
     //     // this.name = nm;
     //     System.out.println(name+" information ");
     // }
-    Student(String name,int id){
+     Student(String name,int id){
         this.name = name;
         this.id = id;
     System.out.println("'Student name is '"+name);
     System.out.println("'Student id is '"+id);
-    }
-}
-public class parameterizedC {
-    public static void main(String[] args){
-        Student s= new Student("anu", 30);
-        // s.st();
     }
 }
