970c7d2dc2fd8b5a337d1a24986e246bcf4c6f75
Inchana A <153069497+Inchanaa@users.noreply.github.com>
2024-12-20 07:00:05 +0000

    bubble sort

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -2,9 +2,10 @@ package Sorting;
 
 import java.util.Scanner;
 
+// time complexity -->  O(n^2)
 public class BubbleSort{
     public static void bubbleSort(int[] arr){
-        for(int i = 0; i<arr.length-1; i++){
+        for(int i = 0; i<arr.length-1; i++){      //n-1
             for(int j=0; j<arr.length-i-1; j++){
                 if(arr[j]>arr[j+1]){
                     int temp = arr[j];
