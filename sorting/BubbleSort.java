bdcaa26cd21c835ed301088cb1cbb60359a13a63
Inchana A <153069497+Inchanaa@users.noreply.github.com>
2024-12-20 07:05:31 +0000

    bubble sort

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -8,9 +8,12 @@ public class BubbleSort{
         for(int i = 0; i<arr.length-1; i++){      //n-1
             for(int j=0; j<arr.length-i-1; j++){
                 if(arr[j]>arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    // int temp = arr[j];
+                    // arr[j] = arr[j+1];
+                    // arr[j+1] = temp;
+                    arr[j] = arr[j] + arr[j+1];
+                    arr[j+1] = arr[j] - arr[j+1];
+                    arr[j] = arr[j] - arr[j+1];
                 }
             }
         }
