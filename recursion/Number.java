c4b90861014b8c9d86acec73518a00357f67f494
Inchana A <153069497+Inchanaa@users.noreply.github.com>
2024-12-20 11:18:12 +0000

    recursion

---
+++ recursion/Number.java
@@ -0,0 +1,23 @@
+package recursion;
+
+import java.util.Scanner;
+
+// print number from n to 1
+
+public class Number {
+public static void printNum(int n){
+    if(n != 0){
+        
+    
+    System.out.println(n);
+    printNum(n-1);
+    }
+}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n value");
+        int n = sc.nextInt();
+        printNum(n);
+        System.out.println();
+    }
+}
