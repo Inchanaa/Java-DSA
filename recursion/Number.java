625c640d052a0ef86595b312cb51dcd8812d4351
Inchana A <153069497+Inchanaa@users.noreply.github.com>
2024-12-23 06:44:38 +0000

    recursion

--- recursion/Number.java
+++ recursion/Number.java
@@ -6,12 +6,12 @@ import java.util.Scanner;
 
 public class Number {
 public static void printNum(int n){
-    if(n != 0){
-        
-    
+    if(n <= 0){
+      return;  
+    }
     System.out.println(n);
     printNum(n-1);
-    }
+    
 }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
