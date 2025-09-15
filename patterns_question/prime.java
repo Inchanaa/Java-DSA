a47b95eb61414d01c7876eadc5f387de01c3f73b
inchanaa <inchanaashok11@gmail.com>
2025-09-15 14:00:57 +0530

    prime number

--- patterns_question/prime.java
+++ patterns_question/prime.java
@@ -1,16 +1,18 @@
 package patterns_question;
+
 import java.util.Scanner;
 
-public class prime{
-    public static void main(String[] args){
+// prime numbers
+public class prime {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-                System.out.println("enter prime number");
-        int n=sc.nextInt();
-        System.out.println("1\n2\n3" );
-        for(int i=4; i<=n; i++){
-        if(i%i==0 && i%2!=0 && i%3!=0 ){
-        System.out.println(i);
-        }
+        System.out.println("enter prime number");
+        int n = sc.nextInt();
+        System.out.println("1\n2\n3");
+        for (int i = 4; i <= n; i++) {
+            if (i % i == 0 && i % 2 != 0 && i % 3 != 0) {
+                System.out.println(i);
+            }
         }
         sc.close();
     }
