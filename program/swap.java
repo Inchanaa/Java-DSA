be613fd9bad4947725435685ad022b8985c8a2d6
inchanaa <inchanaashok11@gmail.com>
2025-09-15 17:16:19 +0530

    swap

--- program/swap.java
+++ program/swap.java
@@ -1,27 +1,27 @@
 // swap a 2 numbers
 
-
 import java.util.Scanner;
 
-public class swap{
-    public static void main(String args[]){
+public class swap {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 2 number to swap ");
         int a = sc.nextInt();
         int b = sc.nextInt();
         int temp = a;
-        System.out.println("temp"+temp);
-        a =  b;
-        System.out.println("a "+a);
-        System.out.println("b "+b);
+        System.out.println("temp= " + temp);
+        a = b;
+        System.out.println("a " + a);
+        System.out.println("b " + b);
         b = temp;
-        System.out.println("b "+b);
-        System.out.println("resylt1" +a+","+b);                //result of swap
+        System.out.println("b " + b);
+        System.out.println("resylt1 = " + a + "," + b); // result of swap
 
         System.out.println("without using temp or 3rd variable");
         a = a + b;
         b = a - b;
         a = a - b;
-        System.out.println("result2 "+a+","+b);                //again swaping the result from above swap
+        System.out.println("result2 = " + a + "," + b); // again swaping the result from above swap
         sc.close();
     }
 }
